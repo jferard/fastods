26b286ff3e03f59a5a98091a2f422415d2f51025
Julien Férard <julien_ferard@yahoo.fr>
2016-06-11 19:30:22 +0200

    Fixed copyright

--- src/main/java/com/github/jferard/fastods/tool/FastOds.java
+++ src/main/java/com/github/jferard/fastods/tool/FastOds.java
@@ -1,15 +1,17 @@
 /*******************************************************************************
  * FastODS - a Martin Schulz's SimpleODS fork
- *    Copyright (C) 2016 J. Férard
+ *    Copyright (C) 2016 J. Férard <https://github.com/jferard>
  * SimpleODS - A lightweight java library to create simple OpenOffice spreadsheets
  *    Copyright (C) 2008-2013 Martin Schulz <mtschulz at users.sourceforge.net>
  * 
- * This program is free software: you can redistribute it and/or modify
+ * This file is part of FastODS.
+ * 
+ * FastODS is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or 
  * (at your option) any later version.
  * 
- * This program is distributed in the hope that it will be useful,
+ * FastODS is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
