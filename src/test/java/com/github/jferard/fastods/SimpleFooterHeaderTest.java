d0aae7f0686d707254675eda370b2ccdce880e04
Julien Férard <julien_ferard@yahoo.fr>
2016-12-07 21:08:18 +0100

    Splitted FooterHeader tests into Simple and Region

--- src/test/java/com/github/jferard/fastods/SimpleFooterHeaderTest.java
+++ src/test/java/com/github/jferard/fastods/SimpleFooterHeaderTest.java
@@ -103,61 +103,6 @@ public class SimpleFooterHeaderTest {
 	}
 
 	@Test
-	public final void testRegionsToMasterStyle() throws IOException {
-		final TextStyle ts1 = TextProperties.builder().fontStyleItalic()
-				.buildStyle("style1");
-		final TextStyle ts2 = TextProperties.builder().fontStyleNormal()
-				.fontWeightNormal().buildStyle("style2");
-		final TextStyle ts3 = TextProperties.builder().fontWeightBold()
-				.buildStyle("style3");
-		final FooterHeader header = FooterHeader
-				.regionBuilder(FooterHeader.Type.HEADER).region(Region.LEFT)
-				.styledContent("left-text", ts1).region(Region.CENTER)
-				.styledContent("center-text", ts2).region(Region.RIGHT)
-				.styledContent("right-text", ts3).build();
-		final StringBuilder sb = new StringBuilder();
-		header.appendXMLToMasterStyle(this.util, sb);
-		DomTester.assertEquals("<style:region-left>" + "<text:p>"
-				+ "<text:span text:style-name=\"style1\">left-text</text:span>"
-				+ "</text:p>" + "</style:region-left>" + "<style:region-center>"
-				+ "<text:p>"
-				+ "<text:span text:style-name=\"style2\">center-text</text:span>"
-				+ "</text:p>" + "</style:region-center>"
-				+ "<style:region-right>" + "<text:p>"
-				+ "<text:span text:style-name=\"style3\">right-text</text:span>"
-				+ "</text:p>" + "</style:region-right>", sb.toString());
-	}
-
-	@Test
-	public final void testRegionToAutomaticStyle() throws IOException {
-		final TextStyle ts = TextProperties.builder().fontWeightBold()
-				.buildStyle("style");
-		final FooterHeader footer = FooterHeader
-				.regionBuilder(FooterHeader.Type.FOOTER).region(Region.CENTER)
-				.styledContent(Text.TEXT_PAGE_NUMBER, ts).build();
-		final StringBuilder sb = new StringBuilder();
-		footer.appendStyleFooterHeaderXMLToAutomaticStyle(this.util, sb);
-		DomTester.assertEquals("<style:footer-style>"
-				+ "<style:header-footer-properties fo:min-height=\"0cm\" fo:margin=\"0cm\"/>"
-				+ "</style:footer-style>", sb.toString());
-	}
-
-	@Test
-	public final void testRegionToMasterStyle() throws IOException {
-		final TextStyle ts = TextProperties.builder().fontWeightBold()
-				.buildStyle("style");
-		final FooterHeader footer = FooterHeader
-				.regionBuilder(FooterHeader.Type.FOOTER).region(Region.CENTER)
-				.styledContent(Text.TEXT_PAGE_NUMBER, ts).build();
-		final StringBuilder sb = new StringBuilder();
-		footer.appendXMLToMasterStyle(this.util, sb);
-		DomTester.assertEquals("<style:region-center>" + "<text:p>"
-				+ "<text:span text:style-name=\"style\">"
-				+ "<text:page-number>1</text:page-number>" + "</text:span>"
-				+ "</text:p>" + "</style:region-center>", sb.toString());
-	}
-
-	@Test
 	public final void testSimpleFooterToAutomaticStyle() throws IOException {
 		final TextStyle ts = TextProperties.builder().fontStyleItalic()
 				.buildStyle("style");
