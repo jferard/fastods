7d63f0941f31156abf2027baf4a2349b3b108183
Julien Férard <julien_ferard@yahoo.fr>
2019-08-02 13:14:27 +0200

    Fix javadoc

--- fastods/src/main/java/com/github/jferard/fastods/attribute/PilotFunction.java
+++ fastods/src/main/java/com/github/jferard/fastods/attribute/PilotFunction.java
@@ -3,7 +3,7 @@ package com.github.jferard.fastods.attribute;
 import com.github.jferard.fastods.attribute.AttributeValue;
 
 /**
- * A custom PilotFunction. Usage: {@code new PilotFunction() { @Override String getAttr() { return "myfunc"; }}.
+ * A custom PilotFunction. Usage: {@code new PilotFunction() { @Override String getAttr() { return "myfunc"; }}}.
  *
  * @author J. Férard
  */
